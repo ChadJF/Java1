fa263f654d84a19fd7a124d626ae41df49ba9ab8
ChadJF <chadjfouts@gmail.com>
2021-03-15 20:00:27 -0500

    Updatihg final

--- src/java_2/oop/podcast/PodcastingApp.java
+++ src/java_2/oop/podcast/PodcastingApp.java
@@ -1,5 +1,8 @@
 package java_2.oop.podcast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class PodcastingApp {
 
 /*
@@ -19,5 +22,52 @@ class PodcastingApp {
        - removeFromPlaylist(Podcast)
 */
 
+    String appName;
+    List<Podcast> playLists = new ArrayList<>();
+
+    public PodcastingApp(String appName) {
+        this.appName = appName;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public void addPodcast(Podcast podcast) {
+        playLists.add(podcast);
+    }
+
+    public void play(PodcastingApp playit) {
+        System.out.println("Playing "+playit.appName);
+    }
+
+    public void stop(PodcastingApp stopit) {
+        System.out.println("Stopping "+stopit.appName);
+    }
+
+    public void next(PodcastingApp nextPC) {
+        System.out.println("Playing next podcast");
+    }
+
+    public void previous(PodcastingApp lastPC) {
+        System.out.println("Playing previous podcast");
+    }
+
+    public void repeat(PodcastingApp loop) {
+        System.out.println("Repeating "+loop.appName);
+    }
+
+    public void displayPodcast(PodcastingApp display) {
+        display.playLists.stream().forEach(show -> System.out.println(show.title));
+    }
+
+    public void remove(Podcast takeAway) {
+        playLists.remove(takeAway);
+
+    }
 }
 
